025c4696835c650ce0172a4921c049e1e7accfbe
Jakob Unnebäck <jakob@folkdatorn.se>
2014-09-08 20:20:07 +0200

    wip
    Major update including a lot of css polishing.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var ejs = require('ejs');
 var http = require('http');
 var express = require('express');
 var socketio = require('socket.io');
@@ -7,21 +8,37 @@ var app = express();
 var server = http.createServer(app);
 var io = socketio(server);
 
+app.set('views', __dirname + '/views');
+app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
 
-io.on('connection', function(socket){
+var messages = [];
+
+io.on('connection', function (socket) {
+
   console.log('a user connected');
-  socket.on('disconnect', function(){
+
+  socket.emit('chat log', messages);
+
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+    messages.push(msg);
+  });
+
+  socket.on('disconnect', function () {
     console.log('a user disconnected');
   });
+
+});
+
+app.get('/', function (req, res) {
+  res.render('page_index.ejs');
+});
+
+app.get('/chat', function (req, res) {
+  res.render('page_chat.ejs');
 });
 
 server.listen(3000, function () {
   console.log('http://localhost:3000');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
 });
